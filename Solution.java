b33143b71c9e3322962fbf6a6673846daf599f3b
Hieu Nguyen <hieunugent@gmail.com>
2020-04-03 20:36:51 -0700

    add solution

---
+++ Solution.java
@@ -0,0 +1,28 @@
+class Solution{
+  public static int [] selectionSort(int [] array){
+    if (array.length == 0){
+      return new int[]{};
+    }
+    int startIdx = 0;
+    while (startIdx < array.length -1 ){
+      int smallestIdx = startIdx;
+      for (int i = startIdx + 1; i < array.legth - 1; i++){
+        if (array[smallestIdx] > array[i]){
+          smallestIdx=i;
+        }
+      }
+      swap(startIdx, smallestIdx, array);
+      startIdx++;
+    }
+
+
+
+
+    return array;
+  }
+  public static void swap( int i , int j , int[] array){
+    int temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+  }
+}
